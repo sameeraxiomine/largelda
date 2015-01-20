da1cc521fcb5e76f193ce0f6ff7bb7e48b047d9c
Sameer Axiomine <sameer@axiomine.com>
2015-01-19 19:30:42 -0500

    update scripts

--- scripts/execlinux.cmd
+++
@@ -1,8 +0,0 @@
-export action=$1
-export MAINDIR=..
-export CLASSPATH=%MAINDIR%/config/:%MAINDIR%/lib/*
-
-java -classpath %CLASSPATH% -Xmx2G  org.bigtextml.client.ParallelTopicModelClient %action%
-
-
-
--- scripts/execwindows.cmd
+++ scripts/execwindows.cmd
@@ -1,6 +1,6 @@
 set action=%1
 set MAINDIR=..
-set CLASSPATH=%MAINDIR%/config/;%MAINDIR%/mylib/*
+set CLASSPATH=%MAINDIR%/config/;%MAINDIR%/lib/*
 java -classpath %CLASSPATH% -Xmx2G  org.bigtextml.client.ParallelTopicModelClient %action%
 
 
